61e1a4a50734f9058137e4accc3794f20b5a5d33
Daniel Dantas <danieldantasoliv@gmail.com>
2021-11-13 22:37:02 -0300

    Proteto innout - Classe Database #02

--- public/index.php
+++ public/index.php
@@ -2,4 +2,12 @@
 
 require_once(dirname(__FILE__, 2) . '/src/config/database.php'); // inportando arquivo com a conexão como banco de dados
 
-Database::getConnection(); // importando o método da clasa database para fazer a conexão
+// Database::getConnection(); // importando o método da clase database para fazer a conexão
+
+$sql = 'select * from users'; // Query de consulta no banco de dados
+$result = Database::getResultfromQuery($sql); // importando o método para executrar a query e colocando na variável $result
+
+while($row = $result->fetch_assoc()) {
+    print_r($row);
+    echo '<br>';
+}
--- src/config/database.php
+++ src/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 class Database { // Criando classe para conecatar com o banco de dados
-    public static function getConnection() {
+    public static function getConnection() { // Método para conrctar no banco de dados
         $envPath = realpath(dirname(__FILE__) . '/../env.ini');
         $env = parse_ini_file($envPath);
         $conn = new mysqli($env['host'], $env['username'], $env['password'], $env['database']);
@@ -11,5 +11,12 @@ class Database { // Criando classe para conecatar com o banco de dados
         }
 
         return $conn;
+    }
+
+    public static function getResultfromQuery($sql) { // Função para fazer uma query(consulta) no banco de dados
+        $conn = self::getConnection();
+        $result = $conn->query($sql);
+        $conn->close();
+        return $result;
     }
 }
