a354fa2730d174400d35eef37d341b244082a68f
Daniel Dantas <danieldantasoliv@gmail.com>
2021-12-09 21:10:25 -0300

    Proteto innout - Tela de Atenticaçao

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php'); // inportando arquivo com a conexão como banco de dados
-require_once(dirname(__FILE__, 2) . '/src/models/User.php'); 
+require_once(dirname(__FILE__, 2) . '/src/views/login.php'); 
 
 // Database::getConnection(); // importando o método da clase database para fazer a conexão
 
@@ -13,15 +13,15 @@ require_once(dirname(__FILE__, 2) . '/src/models/User.php');
     echo '<br>';
 } */
 
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
+// $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
 
 // echo $user->getSelect();
 
-print_r(User::get(['id' => 1], 'id, name, email'));
+// print_r(User::get(['id' => 1], 'id, name, email'));
 
-echo '<br>';
+// echo '<br>';
 
-foreach(User::get([], 'name') as $user) {
-    echo $user->name;
-    echo '<br>';
-}
+// foreach(User::get([], 'name') as $user) {
+//     echo $user->name;
+//     echo '<br>';
+// }
